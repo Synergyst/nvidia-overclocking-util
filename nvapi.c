06751002e629fe927ea13dae9d7dc2f2b9c01fbc
Coleton Moore <root@pococraft.org>
2021-11-19 03:42:29 -0600

    Updated various files

--- nvapi.c
+++ nvapi.c
@@ -25,74 +25,78 @@ NvAPI_GPU_GetAllClockFrequencies_t NvGetFreq = NvGetFreq_trampoline;
 NvAPI_GPU_GetPstates20_t NvGetPstates = NvGetPstates_trampoline;
 NvAPI_GPU_SetPstates20_t NvSetPstates = NvSetPstates_trampoline;
 
-void *NvQueryInterface_trampoline(unsigned int offset)
-{
+void *NvQueryInterface_trampoline(unsigned int offset) {
+    /*
+     * nvapi.dll for 32-bit
+     * IMPORTANT: 64-bit support requires extended reverse-engineering steps as far as I understand to get the 64-bit addresses for each function call.
+     * nvapi64.dll for 64-bit
+     */
     NvQueryInterface = (void*)GetProcAddress(LoadLibrary("nvapi.dll"), "nvapi_QueryInterface");
     return NvQueryInterface(offset);
 }
 
-int NvInit_trampoline(void)
-{
+int NvInit_trampoline(void) {
+    // Address of NvInit found via reverse-engineering steps
     NvInit = NvQueryInterface(0x0150E828);
     return NvInit();
 }
 
-int NvUnload_trampoline(void)
-{
+int NvUnload_trampoline(void) {
+    // Address of NvUnload found via reverse-engineering steps
     NvUnload = NvQueryInterface(0xD22BDD7E);
     return NvUnload();
 }
 
-int NvEnumGPUs_trampoline(int **handles, int *count)
-{
+int NvEnumGPUs_trampoline(int **handles, int *count) {
+    // Address of NvEnumGPUs found via reverse-engineering steps
     NvEnumGPUs = NvQueryInterface(0xE5AC921F);
     return NvEnumGPUs(handles, count);
 }
 
-int NvGetSysType_trampoline(int *handle, int *systype)
-{
+int NvGetSysType_trampoline(int *handle, int *systype) {
+    // Address of NvGetSysType found via reverse-engineering steps
     NvGetSysType = NvQueryInterface(0xBAAABFCC);
     return NvGetSysType(handle, systype);
 }
 
-int NvGetName_trampoline(int *handle, char *sysname)
-{
+int NvGetName_trampoline(int *handle, char *sysname) {
+    // Address of NvGetName found via reverse-engineering steps
     NvGetName = NvQueryInterface(0xCEEE8E9F);
     return NvGetName(handle, sysname);
 }
 
-int NvGetMemSize_trampoline(int *handle, int *memsize)
-{
+int NvGetMemSize_trampoline(int *handle, int *memsize) {
+    // Address of NvGetMemSize found via reverse-engineering steps
     NvGetMemSize = NvQueryInterface(0x46FBEB03);
     return NvGetMemSize(handle, memsize);
 }
 
-int NvGetMemType_trampoline(int *handle, int *memtype)
-{
+int NvGetMemType_trampoline(int *handle, int *memtype) {
+    // Address of NvGetMemType found via reverse-engineering steps
     NvGetMemType = NvQueryInterface(0x57F7CAAC);
     return NvGetMemType(handle, memtype);
 }
 
-int NvGetBiosName_trampoline(int *handle, char *biosname)
-{
+int NvGetBiosName_trampoline(int *handle, char *biosname) {
+    // Address of NvGetBiosName found via reverse-engineering steps
     NvGetBiosName = NvQueryInterface(0xA561FD7D);
     return NvGetBiosName(handle, biosname);
 }
 
-int NvGetFreq_trampoline(int *handle, NV_GPU_CLOCK_FREQUENCIES_V2 *clock_info)
-{
+int NvGetFreq_trampoline(int *handle, NV_GPU_CLOCK_FREQUENCIES_V2 *clock_info) {
+    // Address of NvGetFreq found via reverse-engineering steps
     NvGetFreq = NvQueryInterface(0x0DCB616C3);
     return NvGetFreq(handle, clock_info);
 }
 
-int NvGetPstates_trampoline(int *handle, NV_GPU_PERF_PSTATES20_INFO_V1 *pstates_info)
-{
+int NvGetPstates_trampoline(int *handle, NV_GPU_PERF_PSTATES20_INFO_V1 *pstates_info) {
+    // Address of NvGetPstates found via reverse-engineering steps
     NvGetPstates = NvQueryInterface(0x6FF81213);
     return NvGetPstates(handle, pstates_info);
 }
 
-int NvSetPstates_trampoline(int *handle, NV_GPU_PERF_PSTATES20_INFO_V1 *pstates_info)
-{
+int NvSetPstates_trampoline(int *handle, NV_GPU_PERF_PSTATES20_INFO_V1 *pstates_info) {
+    // Address of NvSetPstates found via reverse-engineering steps
     NvSetPstates = NvQueryInterface(0x0F4DAE6B);
     return NvSetPstates(handle, pstates_info);
 }
--- overclocking.c
+++ overclocking.c
@@ -10,89 +10,113 @@
   0. You just DO WHAT THE FUCK YOU WANT TO.
 */
 
+/*
+ * Some changes (or lack thereof) made to this file include:
+ * 
+ * 
+ * Preserved original author's license
+ * 
+ * Added general comments for future readers (TODO: add even more comments)
+ * 
+ * Fixed some minor issues I've noticed
+ * 
+ * Added getopt.h library to ease implementing as a standalone console app (I have no clue how to reverse-engineer, otherwise this utility would've been a simple DLL)
+ * 
+ * Allowed overclocking either memory AND/OR core, instead of just core OR core AND memory. :)
+ * 
+ * Added GDDR6X support to memory type string list
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
-#include "nvapi.h"
-#include "getopt.h"
 #include <stdbool.h>
+#include "nvapi.h"
+// This include is really only useful for easing implementation for using this overclocking utility as a standalone console application rather than a DLL/library.
+#include "getopt.h"
 
-char* get_memtype_string(int memtype)
-{
-    switch (memtype)
-    {
-    case 0:
-        return "\0";
-        break;
-    case 1:
-        return "SDR";
-        break;
-    case 2:
-        return "DDR";
-        break;
-    case 3:
-    case 9:
-        return "DDR2";
-        break;
-    case 4:
-        return "GDDR2";
-        break;
-    case 5:
-        return "GDDR3";
-        break;
-    case 6:
-        return "GDDR4";
-        break;
-    case 7:
-        return "DDR3";
-        break;
-    case 8:
-        return"GDDR5";
-        break;
-    case 10:
-        return "GDDR5X";
-        break;
-    case 11:
-    case 13:
-    default:
-        printf("[debug] memtype code: %d\n", memtype);
-        return "Unknown memory type";
-        break;
-    case 12:
-        return "HBM2";
-        break;
-    case 14:
-        return "GDDR6";
-        break;
-    case 15:
-        return "GDDR6X";
-        break;
+//#define EXPORT __declspec(dllexport)
+
+char* get_memtype_string(int memtype) {
+    switch (memtype) {
+        case 0:
+            return "\0";
+            break;
+        case 1:
+            return "SDR";
+            break;
+        case 2:
+            return "DDR";
+            break;
+        case 3:
+        case 9:
+            return "DDR2";
+            break;
+        case 4:
+            return "GDDR2";
+            break;
+        case 5:
+            return "GDDR3";
+            break;
+        case 6:
+            return "GDDR4";
+            break;
+        case 7:
+            return "DDR3";
+            break;
+        case 8:
+            return"GDDR5";
+            break;
+        case 10:
+            return "GDDR5X";
+            break;
+        case 11:
+        case 13:
+        default:
+            printf("([debug] memtype code: %d)\t", memtype);
+            return "Unknown memory type";
+            break;
+        case 12:
+            return "HBM2";
+            break;
+        case 14:
+            return "GDDR6";
+            break;
+        case 15:
+            return "GDDR6X";
+            break;
     }
 }
 
-int get_memfreq_multiplier(int memtype)
-{
-    switch (memtype)
-    {
-    case 1:
-    case 2:
-    case 3:
-    case 4:
-    case 5:
-    case 6:
-    case 7:
-    case 9:
-    case 12:
-    default:
-        return 1;
-    case 8:
-        return 2;
-    case 14:
-        return 4;
+int get_memfreq_multiplier(int memtype) {
+    switch (memtype) {
+        case 1:
+        case 2:
+        case 3:
+        case 4:
+        case 5:
+        case 6:
+        case 7:
+        case 9:
+        case 12:
+        default:
+            return 1;
+        case 8:
+            return 2;
+        case 14:
+            return 4;
     }
 }
 
-int main(int argc, char** argv)
-{
+int main(int argc, char** argv) {
+    /*
+     * nGPU = number of GPU's
+     * systype = system type
+     * memsize = capacity
+     * memtype = NVAPI memory type as int
+     * hdlGPU = NVAPI GPU handle array
+     * sysname = Full name of GPU provided by NVAPI
+     * biosname = VBIOS revision provided by NVAPI for given GPU
+     */
     int nGPU = 0;
     int systype;
     int memsize[64], memtype[64];
@@ -101,7 +125,6 @@ int main(int argc, char** argv)
     char* biosname[64];
     NV_GPU_PERF_PSTATES20_INFO_V1 pstates_info[64], pstate_overclock;
     NV_GPU_CLOCK_FREQUENCIES_V2 clock_freq[64];
-
     memset(clock_freq, 0, sizeof(NV_GPU_CLOCK_FREQUENCIES_V2) * 64);
 
     NvInit();
@@ -109,8 +132,7 @@ int main(int argc, char** argv)
     NvGetName(hdlGPU[0], sysname);
     NvGetSysType(hdlGPU[0], &systype);
 
-    for (int i = 0; i < nGPU; i++)
-    {
+    for (int i = 0; i < nGPU; i++) {
         pstates_info[i].version = 0x11c94;
         biosname[i] = malloc(64);
         NvGetMemSize(hdlGPU[i], &memsize[i]);
@@ -122,17 +144,25 @@ int main(int argc, char** argv)
         NvGetPstates(hdlGPU[i], &pstates_info[i]);
     }
 
-    switch (systype)
-    {
-    case 1:     printf("\nType: Laptop\n"); break;
-    case 2:     printf("\nType: Desktop\n"); break;
-    default:    printf("\nType: Unknown\n"); break;
-    }
+    /*
+     * NOTE: This commented block below could be useful for ridding NVML errors in some cases
+     * 
+     * That or perhaps detecting if the user is using a laptop, then producing a debug/warn message
+     * informing them that some laptops may have locked-out controls.
+     * 
+     */
+    /*switch (systype) {
+        case 1:     printf("\nType: Laptop\n"); break;
+        case 2:     printf("\nType: Desktop\n"); break;
+        default:    printf("\nType: Unknown\n"); break;
+    }*/
 
+    /*
+     * NOTE: Uncomment this block below to print debug information without setting a new overclock
+     */
     printf("Name: %s\n", sysname);
     printf("Detected %d GPU(s)\n", nGPU);
-    for (int i = 0; i < nGPU; i++)
-    {
+    for (int i = 0; i < nGPU; i++) {
         printf("Settings for GPU %d\n", i);
         printf("VRAM: %dMB %s\n", memsize[i] / 1024, get_memtype_string(memtype[i]));
         printf("BIOS: %s\n", biosname[i]);
@@ -142,11 +172,15 @@ int main(int argc, char** argv)
         printf("\nCurrent GPU OC: %dMHz\n", (int)((pstates_info[i].pstates[0].clocks[0]).freqDelta_kHz.value) / 1000);
         printf("Current VRAM OC: %dMHz\n\n", (int)((pstates_info[i].pstates[0].clocks[1]).freqDelta_kHz.value) / get_memfreq_multiplier(memtype[i]) / 1000);
     }
+    Sleep(100);
+    exit(0);
 
-    int gpufreq_mhz = 0, cur_gpu = 0, vramfreq_mhz = 0;
+    int cardId = 0, cur_gpu = 0;
+    int gpufreq_mhz = 0, vramfreq_mhz = 0;
     bool doOverclockCore = false, doOverclockMemory = false;
     int opt;
-    while ((opt = getopt(argc, argv, ":i:c:m:")) != -1) {
+    // TODO: this getopt library I'm using works.. though I have it implemented here horrifically. Please ignore/fix/remove/replace with something better. :)
+    while ((opt = getopt(argc, argv, ":i:z:d:c:m:")) != -1) {
         switch (opt) {
             case 'i':
                 cur_gpu = atoi(optarg);
@@ -154,16 +188,37 @@ int main(int argc, char** argv)
                     printf("[GPU #%d] not found!\nPlease specify a GPU within the range 0 to %d", cur_gpu, nGPU - 1);
                     return 0;
                 }
-                printf("Using [GPU #%d]\n", cur_gpu);
+                cardId = optarg;
+                //printf("Using [GPU #%d]\n", cur_gpu);
+                break;
+            case 'z':
+                cardId = atoi(optarg);
+                printf("%d", (int)((pstates_info[cardId].pstates[0].clocks[1]).freqDelta_kHz.value) / get_memfreq_multiplier(memtype[cardId]) / 1000);
+                return 0;
+                break;
+            case 'd':
+                printf("Name: %s\n", sysname);
+                printf("Detected %d GPU(s)\n", nGPU);
+                for (int i = 0; i < nGPU; i++) {
+                    printf("Settings for GPU %d\n", i);
+                    printf("VRAM: %dMB %s\n", memsize[i] / 1024, get_memtype_string(memtype[i]));
+                    printf("BIOS: %s\n", biosname[i]);
+                    free(biosname[i]);
+                    printf("GPU: %dMHz\n", (int)((clock_freq[i].domain[0].frequency) + ((pstates_info[i].pstates[0].clocks[0]).freqDelta_kHz.value)) / 1000);
+                    printf("VRAM: %dMHz\n", (int)((pstates_info[i].pstates[0].clocks[1]).data.single.freq_kHz) / get_memfreq_multiplier(memtype[i]) / 1000);
+                    printf("Current GPU OC: %dMHz\n", (int)((pstates_info[i].pstates[0].clocks[0]).freqDelta_kHz.value) / 1000);
+                    printf("Current VRAM OC: %dMHz\n\n", (int)((pstates_info[i].pstates[0].clocks[1]).freqDelta_kHz.value) / get_memfreq_multiplier(memtype[i]) / 1000);
+                }
+                return 0;
                 break;
             case 'c':
                 gpufreq_mhz = atoi(optarg);
-                printf("Attempting to set [GPU #%d] core offset: %d Mhz\n", cur_gpu, gpufreq_mhz);
+                //printf("Attempting to set [GPU #%d] core offset: %d Mhz\n", cur_gpu, gpufreq_mhz);
                 doOverclockCore = true;
                 break;
             case 'm':
                 vramfreq_mhz = atoi(optarg);
-                printf("Attempting to set [GPU #%d] memory offset: %d Mhz\n", cur_gpu, vramfreq_mhz);
+                //printf("Attempting to set [GPU #%d] memory offset: %d Mhz\n", cur_gpu, vramfreq_mhz);
                 doOverclockMemory = true;
                 break;
             case ':':
@@ -174,7 +229,7 @@ int main(int argc, char** argv)
                 break;
         }
     }
-    if (!(-250 <= gpufreq_mhz && gpufreq_mhz <= 250)) {
+    if (!(-1000 <= gpufreq_mhz && gpufreq_mhz <= 1000)) {
         printf("[core] %d Mhz is out of the safe range!\n", gpufreq_mhz);
         return 0;
     } else {
@@ -188,11 +243,11 @@ int main(int argc, char** argv)
             if (NvSetPstates(hdlGPU[cur_gpu], &pstate_overclock)) {
                 printf("Core OC failed!\n");
             } else {
-                printf("Core OC applied: %d MHz\n", gpufreq_mhz);
+                printf("%d", gpufreq_mhz);
             }
         }
     }
-    if (!(-250 <= vramfreq_mhz && vramfreq_mhz <= 1250)) {
+    if (!(-500 <= vramfreq_mhz && vramfreq_mhz <= 1500)) {
         printf("[memory] %d Mhz is out of the safe range!\n", vramfreq_mhz);
         return 0;
     } else {
@@ -206,10 +261,42 @@ int main(int argc, char** argv)
             if (NvSetPstates(hdlGPU[cur_gpu], &pstate_overclock)) {
                 printf("Memory OC failed!\n");
             } else {
-                printf("Memory OC applied: %d MHz\n", vramfreq_mhz);
+                printf("%d", vramfreq_mhz);
             }
         }
     }
     NvUnload();
     return 0;
 }
+
+/* 
+
+*****
+TODO: Add call to nvidia-smi.exe to lock core clocks.
+Currently this utility will only set offset-based memory and core over/underclocks.
+Power limits can already be set via the public NVAPI (non-NDA version).. so that will not be included in this file.
+*****
+
+EXPORT int SetCoreClockOffset() {
+    HANDLE thread = CreateThread(NULL, 0, ThreadFunc, NULL, 0, NULL);
+    if (thread) {
+        // Optionally do stuff, such as wait on the thread.
+    }
+    printf("\nTesting from DLL\n\n");
+    return 0;
+}
+EXPORT void GetCoreClockOffset() {
+    //
+}
+EXPORT void SetMemoryClockOffset() {
+    //
+}
+EXPORT void GetMemoryClockOffset() {
+    //
+}
+EXPORT void SetPowerLimit() {
+    //
+}
+EXPORT void GetPowerLimit() {
+    //
+}*/
